f0e50b02232035d8a54c04951f64b5b5a0b9c1d0
Robin Genz <mail@robingenz.dev>
2023-02-24 22:55:42 +0100

    wip: ios

--- packages/translation/ios/Plugin/TranslationPlugin.m
+++ packages/translation/ios/Plugin/TranslationPlugin.m
@@ -4,5 +4,8 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(TranslationPlugin, "Translation",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(deleteDownloadedModel, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(downloadModel, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getDownloadedModels, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(translate, CAPPluginReturnPromise);
 )
