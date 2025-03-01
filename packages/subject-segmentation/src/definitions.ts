0f5113bf7acee4d0214a4dfeffdafb2561562d5a
Bruno Perel <bruno.perel@firstvet.com>
2025-03-01 20:53:00 +0100

    Replace @since 7.0.0 with @since 7.2.0

--- packages/subject-segmentation/src/definitions.ts
+++ packages/subject-segmentation/src/definitions.ts
@@ -4,19 +4,19 @@ export interface SubjectSegmentationPlugin {
    *
    * Only available on Android and iOS.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    */
   processImage(options: ProcessImageOptions): Promise<ProcessImageResult>;
 }
 
 /**
- * @since 7.0.0
+ * @since 7.2.0
  */
 export interface ProcessImageOptions {
   /**
    * The local path to the image file.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    */
   path: string;
 
@@ -24,46 +24,46 @@ export interface ProcessImageOptions {
    * Scale the image to this width.
    * If no `height` is given, it will respect the aspect ratio.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    */
   width?: number;
   /**
    * Scale the image to this height.
    * If no `width` is given, it will respect the aspect ratio.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    */
   height?: number;
 
   /**
    * Sets the confidence threshold.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    * @default 0.9
    */
   confidence?: number;
 }
 
 /**
- * @since 7.0.0
+ * @since 7.2.0
  */
 export interface ProcessImageResult {
   /**
    * The path to the segmented image file.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    */
   path: string;
   /**
    * Returns the width of the image file.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    */
   width: number;
   /**
    * Returns the height of the image file.
    *
-   * @since 7.0.0
+   * @since 7.2.0
    */
   height: number;
 }
