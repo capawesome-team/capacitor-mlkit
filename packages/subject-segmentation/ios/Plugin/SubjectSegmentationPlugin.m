1fb2dd0eb1617f1c17a77bd93b28b6faa5021f15
Robin Genz <mail@robingenz.dev>
2025-03-26 10:45:53 +0100

    wip

--- packages/subject-segmentation/ios/Plugin/SubjectSegmentationPlugin.m
+++ packages/subject-segmentation/ios/Plugin/SubjectSegmentationPlugin.m
@@ -5,4 +5,6 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(SubjectSegmentationPlugin, "SubjectSegmentation",
            CAP_PLUGIN_METHOD(processImage, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isGoogleSubjectSegmentationModuleAvailable, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(installGoogleSubjectSegmentationModule, CAPPluginReturnPromise);
 )
